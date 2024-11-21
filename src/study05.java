d81357819e0bd59741206d3a32eebce60308a621
GGobung-of-Simba <j.lim9304@gmail.com>
2024-11-21 15:52:27 +0900

    swich/case문

---
+++ src/study05.java
@@ -0,0 +1,25 @@
+public class study05 {
+
+    public static void main(String[] ar){
+
+        int a = 3;
+
+        switch(a){
+            case 1:
+                System.out.println(1);
+            case 2:
+                System.out.println(2);
+            case 3:
+                System.out.println(3);
+                break;
+            case 4:
+                System.out.println(4);
+            case 5:
+                System.out.println(5);
+            default:
+                System.out.println("others");
+            
+        }
+    }
+    
+}

09c352fdbe0e0e786c3aa7bb20a75c82ba806006
GGobung-of-Simba <j.lim9304@gmail.com>
2024-11-21 14:18:48 +0900

    생성자를 사용하여 출력하기

---
+++ src/Study01.java
@@ -0,0 +1,15 @@
+public class Study01 {
+    
+
+    public static void main(String[] ar) {
+        Study01 study = new Study01();
+        System.out.println(study);
+        int a = study.일의값();
+        System.out.println(a);
+    }
+
+    public int 일의값(){
+        return 1;
+    }
+
+}

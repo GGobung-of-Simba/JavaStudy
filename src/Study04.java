b952aaeaaecec036b883b7ee6ab4818080b7a79b
GGobung-of-Simba <j.lim9304@gmail.com>
2024-11-21 15:41:57 +0900

    if문

---
+++ src/Study04.java
@@ -0,0 +1,63 @@
+public class Study04 {
+    /* 
+     *조건문 if/ else if/ else
+     if문
+     if(조건식) {
+     }
+     
+     else if 문
+     else if(조건식) {
+     }
+
+     else문
+     if(조건식) {
+     } else if (조건식) {
+     }
+
+     * 비교연산자
+     == 같냐고 물어보는 비교자
+     != 같지 않냐고 물어보는 비교자
+     > 앞의 값이 뒤의 값보다 큰지 물어보는 비교자
+     <
+     >=
+     <=
+
+
+     */
+    /*
+     * public static void main(String[]arg){
+        int a = 5;
+        int b = 3;
+
+        if(a == b){//a=b이면 참으로 출력하는 조건문
+            System.out.println("t");
+        }
+        if(a != b){
+            System.out.println("t");
+        }
+        if(a > b){
+            System.out.println("t");
+        }
+        if(a < b){
+            System.out.println("t");
+        }
+
+     */
+    public static void main(String[]arg){
+        int a = 5;
+        int b = 3;
+
+        if(a == b){//a=b이면 참으로 출력하는 조건문
+        } else if (a < b){
+        } else if (a > b){
+        } else if (a > b){
+
+        } else if (a )
+        
+
+
+
+    }
+
+    
+}

144b7c2c5dfd09b1661e6316d42cd7e77e573367
GGobung-of-Simba <j.lim9304@gmail.com>
2024-11-21 14:55:48 +0900

    연산자 알아보기

---
+++ src/Study03.java
@@ -0,0 +1,26 @@
+public class Study03 {
+    /*
+     + 더하기
+     - 빼기
+     * 곱하기
+     / 나누기
+     % 나눈 나머지
+     */
+    public static void main(String[] arg){
+        int a = 3; 
+        int b = 5;
+        int 더하기 = (a+b);//8
+        int 빼기 = (a-b);//-2
+        int 곱하기 = (a * b);//15
+        int 나누기 = (a / b);//0
+        int 나머지 = (a % b);//3
+        
+        System.out.println(더하기);
+        System.out.println(빼기);
+        System.out.println(곱하기);
+        System.out.println(나누기);
+        System.out.println(나머지);
+
+    }
+    
+}

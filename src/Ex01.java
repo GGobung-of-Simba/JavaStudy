1fc05d0ea736d1dfdac4fffe59850589d016d8a7
J.Lim <j.lim9304@gmail.com>
2024-11-22 15:44:22 +0900

    배열문제

--- src/Ex01.java
+++ src/Ex01.java
@@ -4,13 +4,13 @@ public class Ex01 {
 
         String[] a = new String[10];
         for (int i = 0; i <a.length; i++){
-            //System.out.println(i);
+            System.out.println(i);
             if (i%2 == 0){
                 a[i] = "o";
             }else{
                 a[i] = "x";
             }
-            System.out.println(a[i]);
+           // System.out.println(a[i]);
         }
 
     }
---
+++ src/Ex02.java
@@ -0,0 +1,21 @@
+public class Ex02 {
+
+    public static void main(String[] arg){
+        String[][] a = new String [5][3];
+        for(int i=0; i<a.length; i++){
+            for(int j=0; j<a[i].length; j++){
+                System.out.print(i);
+                //System.out.println();
+               /*  if(j == 1){
+                    a[i][j] = (i%2 == 0) ? x : o;
+                }else{
+                    a[i][j] = (i%2 == 0) ? o : x;
+                }
+                System.out.print(a[i][j]);*/
+            }
+
+        }
+        
+    }
+    
+}
